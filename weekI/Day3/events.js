561d4d2c090c0161d924c4df3297c46bb7b6e805
joncomas <joncomas@outlook.com>
2019-06-20 17:33:02 -0400

    MissingForm2

--- weekI/Day3/events.js
+++ weekI/Day3/events.js
@@ -6,41 +6,30 @@ let PaySubmitfunc = function(){
     let hackeableCreditCardNums = document.getElementById('cCardBoxId').value;
     let mahMoney = document.getElementById('AmountBoxId').value;
     let CvCBoxId = document.getElementById('AmountBoxId').value;
-
-    if
-    (
-        
-        
-        
+    let xnombrex = document.getElementById('nombrex').value;
+    let xapellidox = document.getElementById('apellidox').value;
+    let cityx = document.getElementById('xCityx').value;
+    let postmanx = document.getElementById('postman').value;
+    let xtextBoxx = document.getElementById('textBoxx').value;
+    
+    
+    if(      
     validator.isCreditCard(hackeableCreditCardNums) ===  true 
     && 
-    validator.isFloat(mahMoney) === true
+    validator.isCurrency(mahMoney) === true
     &&
     validator.isFloat(CvCBoxId) === true
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    ){ 
-        
-        
+    &&
+    validator.isEmpty(xnombrex) === false
+    &&
+    validator.isEmpty(xapellidox) === false    
+    &&
+    validator.isEmpty(cityx) === false
+    &&
+    validator.isEmpty(postmanx) === false)
+    { 
         alert('Payment Sent')
-
     } else {
-
         alert('Please fill the form correctly!!');
     }
 
